31d3bfc32676382ec053c30dd3f191fbd2b5cf3c
Kyle Miller <millerkyle85@gmail.com>
2022-01-28 18:09:55 -0800

    stuck on logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $("form#language").submit(function(event){
-    let language = 
+    let language = ["whyProgramming", "skillLevel", "typeOfProgram", "newOrOld", "smart"];
+    let userAnswer = 0;
+
+    language.forEach(function(language){
+      userAnswer += parseInt($("select#" + language).val());
+    });
+
     
     
     
