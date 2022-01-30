35b6f0cc7b20b5d67c0b8478a97a14d918c8b8e5
Kyle Miller <millerkyle85@gmail.com>
2022-01-29 19:19:02 -0800

    finished logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
-$(document).ready(function() {
-  $("form#language").submit(function(event){
-    let language = ["whyProgramming", "skillLevel", "typeOfProgram", "newOrOld", "smart"];
+$(document).ready(function () {
+  $("form#language").submit(function (event) {
+    let language = [
+      "whyProgramming",
+      "skillLevel",
+      "typeOfProgram",
+      "newOrOld",
+      "smart",
+    ];
     let userAnswer = 0;
 
-    language.forEach(function(language){
+    language.forEach(function (language) {
       userAnswer += parseInt($("select#" + language).val());
     });
 
-    if (userAnswer <= 3) {
+    if (userAnswer <= 7) {
       $("#python").show();
+      $("#javaScript").hide();
+      $("#ruby").hide();
+    } else if (userAnswer >= 12) {
+      $("#javaScript").show();
+      $("#python").hide();
+      $("#ruby").hide();
+    } else if (userAnswer >= 8 || userAnswer <= 11){
+      $("#ruby").show();
+      $("#python").hide();
+      $("#javaScript").hide();
     }
-
-    
-    
-
-    
-    
-    
-    
     event.preventDefault();
-  
-  
-  
-  
   });
 });
